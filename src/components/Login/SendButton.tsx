710e248d0bd812c95f087736ca1ee7d9a78f7676
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-05-31 15:50:30 -0600

    Remove unused SVG files and login component files

--- src/components/Login/SendButton.tsx
+++
@@ -1,7 +0,0 @@
-export const SendButton = () => {
-  return (
-    <button className="px-4 py-2  border-1 border-secondary font-bold text-lg rounded-sm w-[40%]">
-      Ingresar
-    </button>
-  );
-};
