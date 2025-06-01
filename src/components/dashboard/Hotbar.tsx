025c6f675fc4d6492784be3f9012bd88527b713b
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-06-01 04:12:50 -0600

    Update greeting in Hotbar component to be gender-inclusive

--- src/components/dashboard/Hotbar.tsx
+++ src/components/dashboard/Hotbar.tsx
@@ -11,7 +11,7 @@ export const Hotbar = async () => {
   return (
     <div className="flex justify-around items-center m-6">
       <div className="text-font inline-flex flex-col  justify-center ">
-        <h2 className="text-4xl font-bold">Bienvenido, {name}</h2>
+        <h2 className="text-4xl font-bold">Bienvenido/a, {name}</h2>
         <p className="text-center text-xs font-light mt-1">
           Asi es como van las cosas hasta el momento...
         </p>
