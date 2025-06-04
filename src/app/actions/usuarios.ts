93e18d6e479402337018b9ab8081771b713bf47c
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-06-03 21:40:05 -0600

    Remove unused getAllClients action from usuarios.ts

--- src/app/actions/usuarios.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-import { PrismaClient } from "@prisma/client";
-
-export const getAllClients = async () => {
-  const prisma = new PrismaClient();
-  const usuarios = await prisma.client.findMany();
-
-  return usuarios;
-};
