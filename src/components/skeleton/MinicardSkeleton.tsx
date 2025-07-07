5f9a2d056c61cae0933e7e78eebd7f11d2ed5123
Angel Torres <luis.vidalto@alumno.buap.mx>
2025-07-07 01:23:03 -0600

    Refactor MinicardSkeleton component to accept className prop for customizable styling

--- src/components/skeleton/MinicardSkeleton.tsx
+++ src/components/skeleton/MinicardSkeleton.tsx
@@ -1,11 +1,12 @@
-import { ReactNode } from "react";
 interface Props {
-  children: ReactNode;
+  className?: string;
 }
 
-export const MinicardSkeleton = () => {
+export const MinicardSkeleton = ({ className }: Props) => {
   return (
-    <div className="bg-neutral-600 border-2 border-neutral-600 rounded-lg flex justify-center items-center w-full animate-pulse py-2 font-bold">
+    <div
+      className={`bg-neutral-600 border-2 border-neutral-600 rounded-lg flex justify-center items-center w-full animate-pulse py-2 font-bold ${className}`}
+    >
       <div className="h-5"></div>
     </div>
   );
